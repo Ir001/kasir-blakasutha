25a55812f673c9fc342118d661ee9044d11afe62
Ir001 <irwanantonio2708@gmail.com>
2019-12-25 11:35:45 +0700

    update

--- x.php
+++
@@ -1,8 +0,0 @@
-<?php 
-		$conn = mysqli_connect('localhost', 'root', 'root', 'blakasutha');
-		$sql = "SELECT count(*) as terjual FROM transaksi";
-		$query = $conn->query($sql);
-		$res = $query->fetch_assoc();
-		print_r($res);
-		echo $res['terjual'];
-?>
